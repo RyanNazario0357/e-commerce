d0fa56aac9f6e140a1f439e734a7a610e2166b46
RyanNazario0357 <Ryan0357@icloud.com>
2022-09-08 19:55:59 -0400

    added code to index.js

--- models/index.js
+++ models/index.js
@@ -3,3 +3,11 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 //
+Category.hasMany(Product,{
+    foreignKey: 'category_id'
+});
+//
+Product.belongsTo(Category, {
+    foreignKey: 'category_id'
+});
+//
