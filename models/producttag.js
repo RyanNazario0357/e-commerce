8fe9ef1b5516f91a42796fabbefd78161da13663
RyanNazario0357 <Ryan0357@icloud.com>
2022-09-08 20:43:53 -0400

    finished product tag

--- models/producttag.js
+++ models/producttag.js
@@ -6,7 +6,33 @@ class ProductTag extends Model {}
 ProductTag.init (
     {
         id: {
-            
-        }
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primarykey: true,
+            autoIncrement: true
+        },
+        product_id: {
+            type:DataTypes.INTEGER,
+            references: {
+                model: 'Product',
+                key: 'id'
+            }
+        },
+        tag_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model:'Tag',
+                key:'id'
+            },
+        },
+    },
+    {
+        sequelize, 
+        timestamps:false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product_tag',
     }
-)
+);
+
+module.exports= ProductTag;
