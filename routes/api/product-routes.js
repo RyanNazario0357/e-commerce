0507542f0bd5b4e3a1c11ef2ef4f7c8084919d95
RyanNazario0357 <Ryan0357@icloud.com>
2022-10-27 19:35:16 -0400

    created get one product code

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -11,3 +11,19 @@ router.get('/', async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+  router.get('/:id', async (req, res) => {
+    try {
+      const productData = await Product.findByPk(req.params.id, {
+        include: [{ model: Tag }, { model: Category }]
+      })
+      if (!productData) {
+        res.status(404).json({ message: 'Product could not be found'});
+        return;
+      }
+      res.status(200).json(productData);
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  });
+  
