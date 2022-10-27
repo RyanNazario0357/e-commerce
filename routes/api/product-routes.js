e798498f899dd70e23989639f8860bcde50ba4b2
RyanNazario0357 <Ryan0357@icloud.com>
2022-10-27 19:27:18 -0400

    created get all products code

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,2 +1,13 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
+
+router.get('/', async (req, res) => {
+    try {
+      const productData = await Product.findAll({
+        include: [{ model: Tag }, { model: Category }]
+      });
+      res.status(200).json(productData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
