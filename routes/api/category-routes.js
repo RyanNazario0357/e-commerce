58fd9c7086694e0ea3fe64febede995b976a0a8d
RyanNazario0357 <Ryan0357@icloud.com>
2022-10-27 19:08:29 -0400

    added new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,3 +15,22 @@ router.get('/:id', async (req, res) => {
     const categoryData = await Category.findByPk(req.params.id, {
         include: [{ model: Product }]
     })
+    if (!categoryData) {
+        res.status(404).json({ message: 'No category with this id'});
+        return;
+      }
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  router.post('/', async (req, res) => {
+    try {
+      const categoryData = await Category.create({
+        category_name: req.body.category_name,
+      });
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
