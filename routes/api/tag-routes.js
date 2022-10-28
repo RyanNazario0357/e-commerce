3aad8dc8d0bb32d0b3e0a38a0931af110441e8c9
RyanNazario0357 <Ryan0357@icloud.com>
2022-10-27 20:20:23 -0400

    created single tag finder

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -11,3 +11,18 @@ router.get('/', async (req, res) => {
       res.status(500).json(err)
     }
   });
+
+  router.get('/:id', async (req, res) => {
+    try {
+      const tagData = await Tag.findByPk(req.params.id, {
+        include: [{ model: Product }]
+      })
+      if (!tagData) {
+        res.status(404).json({ message: 'No tag with this id' });
+        return;
+      }
+      res.status(200).json(tagData);
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  });
