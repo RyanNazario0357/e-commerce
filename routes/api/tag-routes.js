a98fb59bce486aab75cf9eb8a04dcc8336b824c8
RyanNazario0357 <Ryan0357@icloud.com>
2022-10-27 20:32:16 -0400

    update tags name by id value

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
         include: [{ model: Product }]
       })
       if (!tagData) {
-        res.status(404).json({ message: 'No tag with this id' });
+        res.status(404).json({ message: 'Unavailable Tag' });
         return;
       }
       res.status(200).json(tagData);
@@ -32,6 +32,23 @@ router.get('/', async (req, res) => {
       const tagData = await Tag.create({
         tag_name: req.body.tag_name,
       });
+      res.status(200).json(tagData);
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  });
+  
+  router.put('/:id', async (req, res) => {
+    try {
+      const tagData = await Tag.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      })
+      if (!tagData[0]) {
+        res.status(404).json({ message: 'Unavailable Tag' });
+        return;
+      }
       res.status(200).json(tagData);
     } catch (err) {
       res.status(500).json(err)
