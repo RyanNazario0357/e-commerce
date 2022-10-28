0b177fa650df4a56730303e93f602f25232f2e66
RyanNazario0357 <Ryan0357@icloud.com>
2022-10-27 20:14:45 -0400

    created find tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,2 +1,13 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
+
+router.get('/', async (req, res) => {
+    try {
+      const tagData = await Tag.findAll({
+        include: [{ model: Product }]
+      });
+      res.status(200).json(tagData);
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  });
